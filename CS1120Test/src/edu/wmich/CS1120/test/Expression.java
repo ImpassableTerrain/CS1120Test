84a679569242966a36d9ed403af7fa75275dc166
Daniel <Daniel@10.80.142.148>
2017-03-01 11:16:10 -0500

    Created new constructor

--- CS1120Test/src/edu/wmich/CS1120/test/Expression.java
+++ CS1120Test/src/edu/wmich/CS1120/test/Expression.java
@@ -2,6 +2,14 @@ package edu.wmich.CS1120.test;
 
 public class Expression {
 	
+	private int one, two;
+	private char a;
 	
+	public Expression (int one, int two, char a) {
+		this.one = one;
+		this.two=two;
+		this.a=a;
+	}
 
+		public Expression(){}  
 }
